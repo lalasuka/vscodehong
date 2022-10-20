58a2faf7971dda896bc452dea6a397a2711382c0
hong <yyyyy12368@gmail.com>
2022-10-20 17:59:39 +0900

    수정

--- children/js/main.js
+++ children/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
         navigation: true, /* 오른쪽에 각 페이지의 paging */
         navigationPosition: 'right', /* 위치 */
-        navigationTooltips: ['메인', '통계', '사업소개', '현장소식', '지원사업'], /* 툴팁 */
+        navigationTooltips: ['메인', '보건 통계', '사업소개', '현장소식', '지원사업'], /* 툴팁 */
         showActiveTooltip: false, /* 현재 활성화된 페이지의 툴팁 보이기 */
 
         // autoScrolling:true, /* 한페이지씩 스크롤 (주지않아도적용된다.) */
@@ -24,15 +24,45 @@ $(document).ready(function(){
                 $('.header').removeClass('black')
                 $('#fp-nav').removeClass('black');
             }
+            if(destination.index == 1 ){ //보건통계 슬라이드가 나타났을때
+                funCount(); //함수 이름이 funCount인 것을 실행하는 명령
+            }
         },
 
         responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
     }); //fullpage
 
-    // count 숫자 count-up
-    // $('.count .rate li .num strong').counterUp({   /* 상세설정을 해주는 경우 */
-    //     delay: 10,   /* 애니메이션이 작동되기 이전 대기 시간 */
-    //     time: 2000    /* 전체 애니메이션 시간 */
-    // });// count-up
+    /* 보건통계 숫자 카운팅 효과 */
+    function funCount(){
+        console.log('카운팅동작')
+        $('.count .rate li .num strong').counterUp({
+            delay: 10,   /* 애니메이션이 작동되기 이전 대기 시간 */
+	        time: 1000    /* 전체 애니메이션 시간 */
+        }); /* 숫자 요소의 클래스명을 써준다. */
+    };
 
-});
+
+    const swiper = new Swiper('.news .list', { /* 팝업을 감싼는 요소의 class명 */
+	    slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	    spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+	    breakpoints: {
+            500: {    /* 500px 이상일때 적용 */
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            800: {    /* 800px 이상일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            1200: {    /* 1200px 이상일때 적용 */
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+	    },   
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.btn_paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+	    },
+    });
+
+});//document.ready
