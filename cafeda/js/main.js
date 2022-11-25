dc5a01c48e1028b82bd8431a531bd2221ad7bbee
hong <yyyyy12368@gmail.com>
2022-11-25 12:56:18 +0900

    수정

--- cafeda/js/main.js
+++ cafeda/js/main.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
     });
 
     const swiper = new Swiper('.event .list', { /* 팝업을 감싼는 요소의 class명 */
-
         slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
@@ -21,6 +20,8 @@ $(document).ready(function(){
               spaceBetween: 20,
           },
         },
+        
+        slidesPerView: "auto",
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
         autoplay: {  /* 팝업 자동 실행 */
