1688cf9275f2c031a7296bd05163c21a2cf1539e
hong <yyyyy12368@gmail.com>
2022-11-25 16:07:13 +0900

    .

--- cafeda/js/main.js
+++ cafeda/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
     const swiper = new Swiper('.event .list', { /* 팝업을 감싼는 요소의 class명 */
         slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        spaceBetween: 120, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
           640: {    /* 640px 이상일때 적용 */
               slidesPerView: 3,
