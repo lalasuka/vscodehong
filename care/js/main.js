b0ab814c23d7cb4d2e9537f5c85a0022c19afc8e
lalasuka <yyyyy12368@gmail.com>
2022-11-27 20:58:12 +0900

    .최종수정

--- care/js/main.js
+++ care/js/main.js
@@ -15,4 +15,27 @@ $(document).ready(function(){
         },
 
     });
+
+    const swiperN = new Swiper('.notice .notice_list', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            500: {    /* 640px 이상일때 적용 */
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            800: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+        },
+        navigation: {
+            nextEl: '.notice .pagin_list .next',
+            prevEl: '.notice .pagin_list .prev',
+        },
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.notice .pagin_list .pagin', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+        },
+    });
 });
